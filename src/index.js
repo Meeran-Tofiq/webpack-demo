7a08c602a8a15213c7d947cb54302024876973a0
Meeran420 <101665499+Meeran420@users.noreply.github.com>
2023-07-13 14:30:27 +0300

    Finish first part of tutorial

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import _ from 'lodash';
 
-function component() {
-    const element = document.createElement('div');
+ function component() {
+   const element = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-}
+  // Lodash, now imported by this script
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+   return element;
+ }
+
+ document.body.appendChild(component());
